7f394e44473bf039f068ed5ddefeaa9145f55b03
shimshamz <sm2292@njit.edu>
2017-11-04 16:46:07 -0400

    first commit

---
+++ users_db.php
@@ -0,0 +1,51 @@
+<?php
+class UsersDB {
+
+	private function __construct() {}
+
+	public static function getUsers() {
+
+		$query = "SELECT * FROM accounts";
+		$statement = Database::getDB()->prepare($query); 
+		$statement->execute();
+		$accounts = $statement->fetchAll();
+		$usersArray = array();
+
+		foreach ($accounts as $account) {
+				
+			$usersArray[] = new User($account['id'], $account['email'], $account['fname'], $account['lname'], $account['phone'], 
+				$account['birthday'], $account['gender'], $account['password']);
+
+		}
+
+
+		return $usersArray;
+	
+	}
+
+	public static function insertUser($email, $fname, $lname, $phone, $birthday, $gender, $password) {
+
+		$query = "INSERT INTO accounts (email, fname, lname, phone, birthday, gender, password)
+					VALUES ('$email', '$fname', '$lname', '$phone', '$birthday', '$gender', '$password')";
+		$statement = Database::getDB()->prepare($query); 
+		$statement->execute();
+
+	}
+
+	public static function updateUserPassword($id, $password) {
+
+		$query = "UPDATE accounts SET password = '$password' WHERE id = '$id'";
+		$statement = Database::getDB()->prepare($query); 
+		$statement->execute();
+
+	}
+
+	public static function deleteUser($id) {
+
+		$query = "DELETE FROM accounts WHERE id = '$id'";
+		$statement = Database::getDB()->prepare($query); 
+		$statement->execute();
+
+	}
+}
+?>
